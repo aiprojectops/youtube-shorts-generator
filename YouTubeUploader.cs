cf275308bdc175962a44dc7cfca748513c72baab
aiprojectops <ai.project.ops@gmail.com>
2025-10-27 12:10:49 +0900

    Update YouTubeUploader.cs

--- YouTubeUploader.cs
+++ YouTubeUploader.cs
@@ -604,36 +604,4 @@ namespace YouTubeShortsWebApp
         }
     }
 
-    // 메모리 데이터 저장소 클래스
-    public class MemoryDataStore : IDataStore
-    {
-        private static readonly ConcurrentDictionary<string, object> _store = new();
-
-        public Task StoreAsync<T>(string key, T value)
-        {
-            _store[key] = value;
-            return Task.CompletedTask;
-        }
-
-        public Task DeleteAsync<T>(string key)
-        {
-            _store.TryRemove(key, out _);
-            return Task.CompletedTask;
-        }
-
-        public Task<T> GetAsync<T>(string key)
-        {
-            if (_store.TryGetValue(key, out object value) && value is T)
-            {
-                return Task.FromResult((T)value);
-            }
-            return Task.FromResult(default(T));
-        }
-
-        public Task ClearAsync()
-        {
-            _store.Clear();
-            return Task.CompletedTask;
-        }
-    }
 }
